ed15437bea475b9348cb51e3ba1000c854985de6
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-08 19:48:32 +0530

    hero carousel

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -5,7 +5,11 @@ const Searchbar = () => {
 	return (
 		<form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
 			<input type="text" className="searchbar-input" />
-			<button type="submit" className="searchbar-btn">
+			<button
+				type="submit"
+				placeholder="Enter Product Link"
+				className="searchbar-btn"
+			>
 				Search
 			</button>
 		</form>
