b53f8ee1064eb421a56a5d68261e153b03ad5167
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-08 21:23:34 +0530

    scraper setup bright

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,16 +1,54 @@
 "use client";
 
+import { FormEvent, useState } from "react";
+
+const isValidAmazonLink = (url: string) => {
+	try {
+		const parsedUrl = new URL(url);
+		const hostname = parsedUrl.hostname;
+
+		if (
+			hostname.includes("amazon.com") ||
+			hostname.includes("amazon.") ||
+			hostname.includes("amazon")
+		) {
+			return true;
+		}
+	} catch (error) {
+		return false;
+	}
+	return false;
+};
+
 const Searchbar = () => {
-	const handleSubmit = () => {};
+	const [searchPrompt, setSearchPrompt] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const validLnik = isValidAmazonLink(searchPrompt);
+		if (!validLnik) return alert("enter a valid amazon link");
+		try {
+			setIsLoading(true);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 	return (
 		<form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
-			<input type="text" className="searchbar-input" />
+			<input
+				type="text"
+				className="searchbar-input"
+				onChange={(e) => setSearchPrompt(e.target.value)}
+			/>
 			<button
 				type="submit"
 				placeholder="Enter Product Link"
 				className="searchbar-btn"
+				disabled={searchPrompt === ""}
 			>
-				Search
+				{isLoading ? "Searching..." : "Search"}
 			</button>
 		</form>
 	);
