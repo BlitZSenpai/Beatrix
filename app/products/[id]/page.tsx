615a6ac4655a787ce77864edd0096c307d2283c0
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-10 23:24:13 +0530

    priceinfocard and cards

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PriceInfoCard from "@/components/PriceInfoCard";
 import { getProductById } from "@/lib/actions";
 import { formatNumber } from "@/lib/utils";
 import { Product } from "@/types";
@@ -106,6 +107,38 @@ const ProductDetails = async ({ params: { id } }: Props) => {
 									<span className="text-primary-green font-semibold">95%</span>{" "}
 									of buyers have recommeneded this.
 								</p>
+							</div>
+						</div>
+						<div className="my-7 flex flex-col gap-5">
+							<div className="flex flex-wrap gap-5">
+								<PriceInfoCard
+									title="Current Price"
+									iconSrc="/assets/icons/price-tag.svg"
+									value={`${product.currency} ${formatNumber(
+										product.currentPrice
+									)}`}
+								/>
+								<PriceInfoCard
+									title="Average Price"
+									iconSrc="/assets/icons/chart.svg"
+									value={`${product.currency} ${formatNumber(
+										product.averagePrice
+									)}`}
+								/>
+								<PriceInfoCard
+									title="Highest Price"
+									iconSrc="/assets/icons/arrow-up.svg"
+									value={`${product.currency} ${formatNumber(
+										product.highestPrice
+									)}`}
+								/>
+								<PriceInfoCard
+									title="Lowest Price"
+									iconSrc="/assets/icons/arrow-down.svg"
+									value={`${product.currency} ${formatNumber(
+										product.lowestPrice
+									)}`}
+								/>
 							</div>
 						</div>
 					</div>
